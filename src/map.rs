ec35c019bade277f98a3387f827601c255dcc21a
cam <cameron@catchdesign.co.nz>
2022-09-21 21:03:00 +1200

    ecs refactor

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,4 @@
-use crate::{camera, prelude::*};
+use crate::prelude::*;
 const NUM_TITLES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
 #[derive(Copy, Clone, PartialEq)]
@@ -32,38 +32,6 @@ impl Map {
             None
         } else {
             Some(map_idx(point.x, point.y))
-        }
-    }
-
-    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
-        ctx.set_active_console(0);
-
-        for y in camera.top_y..camera.bottom_y {
-            for x in camera.left_x..camera.right_x {
-                if self.in_bounds(Point::new(x, y)) {
-                    let idx = map_idx(x, y);
-                    match self.tiles[idx] {
-                        TileType::Floor => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                YELLOW,
-                                BLACK,
-                                to_cp437('.'),
-                            );
-                        }
-                        TileType::Wall => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                GREEN,
-                                BLACK,
-                                to_cp437('#'),
-                            );
-                        }
-                    }
-                }
-            }
         }
     }
 }
